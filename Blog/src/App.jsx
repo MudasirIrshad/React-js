fc18b69738cc191f6a5a336c7d7a4d9470da9be1
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-10-01 16:06:32 +0500

    first react blog page

---
+++ Blog/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='maindiv shadow'>
+        <div>
+          <h1>Hello Everyone</h1>
+          <div className="circular-image shadow">
+            <img src="/img.jpg" alt="" />
+        </div>
+        </div>
+        <div className='para'>
+       <b>Mudasir Irshad</b> <br/>
+
+        In the world of software development, your proficiency in SQL for effective data management, HTML and CSS for web content structure and design, JavaScript for dynamic web interactivity, and TypeScript for enhanced code quality is the key to becoming a well-rounded developer. These essential skills empower you to build powerful web applications, manage databases efficiently, and adapt to the ever-changing tech landscape, offering a pathway to exciting career opportunities and the ability to create digital solutions that make a real impact. So, Mudasir Irshad, dive in, start learning, and embark on your journey to becoming a versatile software developer. 🚀 <br /><br />
+
+        My GitHub profile: <a href="https://github.com/MudasirIrshad">Click to access</a>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
