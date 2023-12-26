14fb4854f1fd1eb1ea1194c51a41117b419fb0cc
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-12-27 00:47:50 +0500

    Minor changes

--- School_rating_system/src/Components/SchoolLists/School.jsx
+++ School_rating_system/src/Components/SchoolLists/School.jsx
@@ -47,7 +47,7 @@ export default function RecipeReviewCard(props) {
           </IconButton>
         }
         title={props.name}
-        subheader="Rating  4/5"
+        subheader={`Rating: ${props.rating}`}
       />
       <CardMedia
         component="img"
@@ -62,7 +62,7 @@ export default function RecipeReviewCard(props) {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton>
-        <button className='bg-black p-1 m-1 rounded border border-gray-300 text-white text-xs'><Link to='/admission'>Admissions</Link></button>
+        <button className='bg-blue-500 p-1 m-1 rounded border border-gray-300 text-white text-xs'><Link to='/admission'>Admissions</Link></button>
         </IconButton>
         <ExpandMore
           expand={expanded}
@@ -70,35 +70,23 @@ export default function RecipeReviewCard(props) {
           aria-expanded={expanded}
           aria-label="show more"
         >
-          <ExpandMoreIcon />
+          <ExpandMoreIcon/>
         </ExpandMore>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>Detail:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nemo eveniet! Tempore, ad minus obcaecati inventore pariatur consectetur culpa illum veniam, eligendi delectus incidunt ratione?
           </Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae animi molestias ipsum ipsam. Mollitia praesentium hic excepturi suscipit eaque tenetur corrupti, perspiciatis illo, provident beatae eum ratione quo facere.
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore consectetur delectus id qui doloremque totam vero quo eos sapiente cumque adipisci, rem consequatur illo quidem suscipit obcaecati explicabo quia eius?
           </Typography>
           <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+            Lorem ipsum dolor sit amet.
           </Typography>
         </CardContent>
       </Collapse>
--- School_rating_system/src/Components/SchoolLists/SchoolList.jsx
+++ School_rating_system/src/Components/SchoolLists/SchoolList.jsx
@@ -21,12 +21,12 @@ export default function SchoolList() {
             flexDirection: 'column',
           }
       }}>
-      <School name="Pakturk International Marif" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="red"/>
-      <School name="Fauji Foundation Model School" img="src\Components\SchoolLists\Images\FFMS.jpg" color="purple"/>
-      <School name="Beacon House Schools" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="green"/>
-      <School name="Islamia Model High School" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="red"/>
-      <School name="St Francis Schooling System" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="purple"/>
-      <School name="Pakturk International Marif" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="green"/>
+      <School name="Pakturk International Marif" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="red" rating='4.5/5'/>
+      <School name="Fauji Foundation Model School" img="src\Components\SchoolLists\Images\FFMS.jpg" color="purple" rating='4/5'/>
+      <School name="Beacon House Schools" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="green" rating='3.3/5'/>
+      <School name="Islamia Model High School" img="src\Components\SchoolLists\Images\FFMS.jpg" color="red" rating='4/5'/>
+      <School name="St Francis Schooling System" img="src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="purple" rating='3/5'/>
+      <School name="Pakturk International Marif" img="src\Components\SchoolLists\Images\FFMS.jpg" color="green" rating='3.5/5'/>
 
       
         </div>
